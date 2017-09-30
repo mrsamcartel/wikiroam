4af430d0b3a8fcc101c9b693f13f66a7e71a8b7f
mrsamcartel <mrsamcartel@gmail.com>
2017-09-30 17:25:19 -0400

    Integrate Datadog monitoring in API layer

--- wikipage-service/api/wikipages.js
+++ wikipage-service/api/wikipages.js
@@ -6,8 +6,22 @@ const AWS = require('aws-sdk');
 AWS.config.setPromisesDependency(require('bluebird'));
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const headers = {
+  "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+  "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+}
+
+const datalog = (metric_name, metric_type='count', metric_value=1, tags=[]) => {
+  // MONITORING|unix_epoch_timestamp|metric_value|metric_type|my.metric.name|#tag1:value,tag2
+  const timestamp = new Date().getTime();
+  tags.push(`service:${process.env.SERVICE}`);
+  tags.push(`stage:${process.env.STAGE}`);
+  var log_entry = `MONITORING|${timestamp}|${metric_value}|${metric_type}|${process.env.LOG_PREFIX}.${metric_name}|#${tags.join(',')}`;
+  console.log(log_entry);
+}
 
 // wikipagesSubmit function
+//TODO: Refactor to align structure with other functions within the service
 module.exports.submit = (event, context, callback) => {
   const requestBody = JSON.parse(event.body);
   const title = requestBody.title;
@@ -19,39 +33,37 @@ module.exports.submit = (event, context, callback) => {
     return;
   }
 
-  // const response = {
-  //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     message: 'Go Serverless v1.0! Your function executed successfully!',
-  //     input: event,
-  //   }),
-  // };
-  // callback(null, response);
-
   //TODO: Avoid storing duplicate pageids; retrieve id and updateItem instead
   submitWikipageP(wikipageInfo(pageid, title))
     .then(res => {
-      callback(null, {
+      const response = {
         statusCode: 200,
+        headers: headers,
         body: JSON.stringify({
           message: `Sucessfully submitted wikipage with pageid ${pageid} and title ${title}`,
           wikipageId: res.id
         })
-      });
+      }
+      datalog('db.responses', undefined, undefined,['status:200'])
+      callback(null, response);
     })
     .catch(err => {
       console.log(err);
-      callback(null, {
+      const response = {
         statusCode: 500,
+        headers: headers,
         body: JSON.stringify({
           message: `Unable to submit candidate with pageid ${pageid} and title ${title}`
         })
-      })
-    });
+      };
+      datalog('db.responses', undefined, undefined,['status:500'])
+      callback(null, response);
+    })
 };
 
 const submitWikipageP = wikipage => {
   console.log('Submitting wikipage');
+  datalog('db.queries.put');
   const wikipageInfo = {
     TableName: process.env.WIKIPAGE_TABLE,
     Item: wikipage,
@@ -79,15 +91,19 @@ module.exports.list = (event, context, callback) => {
   };
 
   console.log("Scanning Wikipage table.");
+  datalog('db_scans');
 
   const onScan = (err, data) => {
     if (err) {
       console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+      datalog('db.responses', undefined, undefined,['status:500'])
       callback(err);
     } else {
       console.log("Scan succeeded.");
+      datalog('db.responses', undefined, undefined,['status:200'])
       return callback(null, {
         statusCode: 200,
+        headers: headers,
         body: JSON.stringify({
           wikipages: data.Items
         })
@@ -117,15 +133,19 @@ module.exports.get = (event, context, callback) => {
   }
 
   console.log(`Getting Wikipage item with id: ${id}.`);
+  datalog('db.queries.get');
 
   const onGet = (err, data) => {
     if (err) {
       console.log('Get failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+      datalog('db.responses', undefined, undefined,['status:500'])
       callback(err);
     } else {
       console.log("Get succeeded.");
+      datalog('db.responses', undefined, undefined,['status:200'])
       return callback(null, {
         statusCode: 200,
+        headers: headers,
         body: JSON.stringify(data.Item)
       });
     }
@@ -163,14 +183,20 @@ module.exports.update = (event, context, callback) => {
     ReturnValues: 'ALL_NEW',
   }
 
+  console.log(`Updating Wikipage item with id: ${id}.`);
+  datalog('db.queries.update');
+
   const onUpdate = (err, data) => {
     if (err) {
       console.log('Put failed to update data. Error JSON:', JSON.stringify(err, null, 2));
+      datalog('db.responses', undefined, undefined,['status:500'])
       callback(err);
     } else {
       console.log("Put succeeded.");
+      datalog('db.responses', undefined, undefined,['status:200'])
       return callback(null, {
         statusCode: 200,
+        headers: headers,
         body: JSON.stringify(data.Attributes),
       });
     }
