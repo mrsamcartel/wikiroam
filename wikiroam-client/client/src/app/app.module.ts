06e6036a5b3c51b8bad760f7607947933db60b94
mrsamcartel <mrsamcartel@gmail.com>
2017-10-01 17:21:02 -0400

    Add basic theming

--- wikiroam-client/client/src/app/app.module.ts
+++ wikiroam-client/client/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule }    from '@angular/http';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material';
+
 import { WikipageService } from './services/wikipage.service';
 
 import { AppComponent } from './app.component';
@@ -14,7 +17,9 @@ import { WikipageComponent } from './wikipage/wikipage.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [WikipageService],
   bootstrap: [AppComponent]
